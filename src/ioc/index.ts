47d1451829813513a6cce213f6c3eacf1676c67e
tpanfilov <tpanfilov@outwardinc.com>
2024-04-11 19:37:31 +0300

    tpanfilov - task

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,29 @@
 import IoCContainer from 'ioc-lite';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
+import { ApiConfig } from '../types';
+import { getConfig } from '../services/config';
+
+
+export type IOCType = {
+  config: ApiConfig,
+  http: typeof HTTP,
+  logger: typeof Logger,
+  users: typeof Users,
+};
+
+export type IOCNames = keyof IOCType
+
 
 export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
-  // you can register some resources right now below...
-
+  const ioc = new IoCContainer<IOCType>();
+  const config = getConfig();
+  
+  ioc.register('config', config.api)
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('users', Users)
   return ioc;
 };
